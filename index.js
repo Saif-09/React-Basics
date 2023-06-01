ea3b0ae5510baccff82c5aad9d5effca8bb5166b
Saif-09 <saifmd238@gmail.com>
2023-06-01 12:41:02 +0530

    creating a component

--- index.js
+++ index.js
@@ -15,11 +15,16 @@
 
 /* REACT with JSX */
 
-const jsxHeading = (
-  <React.Fragment>
-    <h1 className="head">Hello JSX </h1>
-    <p>This is created using JSX</p>
-  </React.Fragment>
-);
 
-ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+//This is the method of creating a component in react, naming convention of component should be like first letter will be capital
+  function App() {
+    return (
+      <>
+        <h1 className="head">Hello JSX </h1>
+        <p>This is created using JSX</p>
+      </>
+    )
+  }
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(App());
